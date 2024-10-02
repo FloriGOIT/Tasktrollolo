907987c1ea2a15473f9792a27a06504f65d074da
exigibil <adrian.topa04@gmail.com>
2024-10-02 23:10:59 +0300

    calendar style

--- src/Components/Custominput/PrimeCalendar.jsx
+++ src/Components/Custominput/PrimeCalendar.jsx
@@ -2,13 +2,14 @@ import "primereact/resources/themes/saga-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import React from "react";
 import { Calendar } from "primereact/calendar";
+import "./styles.css";
 
 export default function CalendarPicker({ value, onChange }) {
     const today = new Date();
     const maxDate = new Date(today.getFullYear() + 5, 11, 31);
 
     return (
-        <div className="card flex justify-content-center">
+        <div className="calendarStyle">
             <Calendar
                 value={value}
                 onChange={onChange}
@@ -20,6 +21,7 @@ export default function CalendarPicker({ value, onChange }) {
                 yearNavigator
                 yearRange="1900:2100"
                 appendTo={document.body}
+                style={{ width: "100%" }}
             />
         </div>
     );
